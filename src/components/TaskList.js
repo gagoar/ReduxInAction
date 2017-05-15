2b06ed701b49cde029a321ff03d3041f28723e40
gagoar <xeroice@gmail.com>
2017-05-14 17:38:53 -0700

    Listing 2.11

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,16 +1,68 @@
 import React, { Component } from 'react';
-import Task from './Task';
+import Task from '../components/Task';
+
+const style = {};
+
+const formStyle = {};
 
 class TaskList extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.tasks.map(task => (
-          <Task key={task.id} task={task} />
-        ))}
-      </div>
-    );
-  }
+ constructor(props) {
+   super(props);
+   this.state = {
+     showNewCardForm: false,
+     title: '',
+     description: '',
+   };
+ }
+
+ onTitleChange = (e) => {
+   this.setState({ title: e.target.value });
+ }
+
+ onDescriptionChange = (e) => {
+   this.setState({ description: e.target.value });
+ }
+
+ onSubmit = () => {
+   this.props.onSubmit({
+     title: this.state.title,
+     description: this.state.description,
+   });
+
+   this.setState({
+     showNewTaskForm: false,
+     title: '',
+     description: '',
+   });
+ }
+
+ render() {
+   return (
+     <div style={style.cards}>
+       <div style={style.newCard}>
+         <span
+           style={style.newCardText}
+           onClick={() => this.setState({ showNewTaskForm: true })}
+         >
+           + New card
+         </span>
+       </div>
+       {this.state.showNewTaskForm && (
+         <form onSubmit={this.onSubmit}>
+           <input onChange={this.onTitleChange} value={this.state.title} style={formStyle.formControlFullWidth} type="text" placeholder="title" />
+           <input onChange={this.onDescriptionChange} value={this.state.subscription} style={formStyle.formControlFullWidth} type="text" placeholder="description" />
+           <button
+             style={{marginBottom: '10px'}}
+             type="submit"
+           >
+             Save
+           </button>
+         </form>
+       )}
+       {this.props.tasks.map(task => <Task key={task.id} task={task}  />)}
+     </div>
+   );
+ }
 }
 
 export default TaskList;
