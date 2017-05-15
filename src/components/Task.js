217e4ed5182cac326b2ad8ff69fc141d0a22550e
Gago <gago@eventbrite.com>
2017-05-15 12:01:40 -0700

    Listing 2.17 src/components/Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
-const Task = (props) => (
-    <div className="task">
-      <div className="task-header">
-        <div>{props.task.title}</div>
-      </div>
-      <hr />
-      <div className="task-body">{props.task.description}</div>
-    </div>
-  );
+const TASK_STATUSES = [
+    "Unstarted",
+    "In Progress",
+    "Completed"
+];
 
-  export default Task;
+const Task = (props) => {
+    return (
+        <div className="task">
+            <div className="task-header">
+                <div>{props.task.title}</div>
+                <select value={props.task.status}>
+                    {TASK_STATUSES.map(status => (
+                        <option key={status} value={status}>{status}</option>
+                        ))}
+                    </select>
+                </div>
+                <hr />
+                <div className="task-body">{props.task.description}</div>
+            </div>
+    );
+}
+
+export default Task;
