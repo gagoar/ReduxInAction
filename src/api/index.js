74004e24caf577ac5202bb91ef80f433c1198fb7
Gago <gago@eventbrite.com>
2017-07-11 19:27:50 -0700

    Listing 4.6 src/api/index.js

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+
+const API_BASE_URL = 'http://localhost:3001';
+
+const client = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const fetchTasks = () => client.get('/tasks');
