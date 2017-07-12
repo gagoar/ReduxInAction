916c85039dbb64a6ca323a0da23c7a247af31130
Gago <gago@eventbrite.com>
2017-07-11 20:03:33 -0700

    Listing 4.8 src/reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,4 @@
-const initialState = [
- {
-   id: 1,
-   title: 'Learn Redux',
-   description: 'The store, actions, and reducers, oh my!',
-   status: 'In Progress',
- },
- {
-   id: 2,
-   title: 'Peace on Earth',
-   description: 'No big deal.',
-   status: 'In Progress',
- },
-];
-
-export default function tasks(state = initialState, action) {
+export default function tasks(state = [], action) {
     if (action.type === 'CREATE_TASK') {
         return state.concat(action.payload);
     }
@@ -32,6 +17,10 @@ export default function tasks(state = initialState, action) {
             return task;
         })
     }
+
+    if (action.type === 'FETCH_TASKS_SUCCEEDED') {
+        return action.payload;
+    }
+
     return state;
 }
-
