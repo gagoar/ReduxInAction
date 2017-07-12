2446d7f8f6212a86373908c2c073b031709080f4
Gago <gago@eventbrite.com>
2017-07-11 19:18:40 -0700

    Listing 4.2 src/store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 export function configureStore(reducer) {
-   return createStore(reducer, devToolsEnhancer());
+    return createStore(
+        reducer,
+        devToolsEnhancer(),
+        applyMiddleware(thunk)
+    );
 }
 
